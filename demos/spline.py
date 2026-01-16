88761b81aa4c13bb1ac401fba007923c7049711a
Marc Toussaint <toussaint@tu-berlin.de>
2026-01-16 10:28:06 +0100

    tutorials tested

--- demos/spline.py
+++ demos/spline.py
@@ -11,7 +11,7 @@ plt.plot(B)
 plt.show()
 
 ## setting random control points, and fine-evaluating the resulting spline
-X = np.random.randn(10,1)
+X = np.random.randn(10,3)
 S.setCtrlPoints(X)
 Teval = np.linspace(-.1,1.1,100)
 x = S.eval(Teval)
@@ -23,7 +23,7 @@ plt.plot(Teval, np.hstack((x,v/10.,a/100.)))
 plt.plot(T_X, X, 'p')
 plt.show()
 
-## computing optimal control points that fix the random data X from above
+## computing optimal control points that fit the random data X from above
 B = S.getBmatrix(T_X, False, True) #start not constrained to zero vel, end is
 Z = np.linalg.pinv(B) @ X
 S.setCtrlPoints(Z, False, True)
